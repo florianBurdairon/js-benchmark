8f78d65856dd901780d33a4d80d3c545126f5661
Energisse <thomas.halvick@gmail.com>
2025-02-13 02:10:06 +0100

    ptn

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import express from 'express';
-import pokemonsRoutes from './routes/pokemons.js'; // Importation des routes
+import express from "express";
+import pokemonsRoutes from "./routes/pokemons.js"; // Importation des routes
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/api/pokemons', pokemonsRoutes);
+app.use("/api/pokemons", pokemonsRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
