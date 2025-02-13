bc0c1d72bf9b22dbee5d55135bbe0b0cd72bb2c3
Florian Burdairon <florian.burdairon@gmail.com>
2025-02-13 02:35:23 +0100

    benchmark

--- benchmark.js
+++ benchmark.js
@@ -1,11 +1,21 @@
 console.time("fetching 1000 times");
-await fetch("https://localhost:3000/api/pokemons/1");
+for (let i = 0; i < 100; i++) {
+  await fetch("http://localhost:3000/api/pokemons", { method: "GET" });
+}
 console.timeEnd("fetching 1000 times");
 
-// console.time("fetching 1000 times");
-// for (let i = 0; i < 1; i++) {
-//   await fetch("http://localhost:3000/api/pokemons", { method: "GET" }).then(
-//     (res) => console.log(res) || res.json()
-//   );
-// }
-// console.timeEnd("fetching 1000 times");
+const pool = 100;
+
+let request = 10000;
+
+function fetch2() {
+    request--;
+  return fetch("http://localhost:3000/api/pokemons", { method: "GET" }).then(() => {
+    if (request > 0) {
+      return fetch2();
+    }
+  });
+}
+console.time("fetching 1000 times2");
+await Promise.all(Array.from({ length: pool }, () => fetch2()));
+console.timeEnd("fetching 1000 times2");
